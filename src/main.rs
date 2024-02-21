c329f7e40bbb3c294eb9c1089ee99b3afe03c2ed
Junior Grégoire <junior.gregoire@gmail.com>
2024-02-21 02:26:06 -0500

    Populate ingredient list

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,26 @@ use rocket_dyn_templates::{Template, context};
 use rocket::fs::{FileServer, relative};
 use rocket::form::Form;
 use rocket::State;
+use rocket::response::Redirect;
 
 mod database;
 mod models;
+mod ingredient_list_manager;
 mod recipes_printer;
 
 use models::*;
 use database::Database;
+use ingredient_list_manager::*;
 
 #[derive(Responder)]
 #[response(status = 200, content_type = "pdf")]
 struct IncludedPdf(Vec<u8>);
 
 #[post("/recipes/new", data = "<recipe>")]
-async fn new_recipe(db: &State<Database>, recipe: Form<Recipe>) -> String {
+async fn new_recipe(db: &State<Database>, recipe: Form<Recipe>) -> Redirect {
     let response = format!("Add Recipe: {}, Preps: {:?}, ingredients: {:?}", recipe.title, recipe.preparation_steps, recipe.ingredients);
     db.recipe.create(&recipe.into_inner()).await;
-    response
+    Redirect::to(uri!(add_recipe))
 }
 
 #[get("/recipes")]
@@ -29,8 +32,9 @@ async fn recipes(db: &State<Database>) -> Template {
 }
 
 #[delete("/recipes/<id>")]
-async fn delete_recipe(db: &State<Database>, id: &str) {
+async fn delete_recipe(db: &State<Database>, id: &str) -> Redirect {
     db.recipe.delete(id).await;
+    Redirect::to(uri!(recipes))
 }
 
 #[get("/recipes/add")]
@@ -65,14 +69,25 @@ async fn get_recipe_pdf(db: &State<Database>, id: &str) -> IncludedPdf {
 #[get("/ingredients_list")]
 async fn ingredients_list(db: &State<Database>) -> Template {
     let allergens = db.allergen.get_all().await;
+    let recipes = db.recipe.get_all().await;
+    let ingredients_list = get_ingredients_list(&recipes, &allergens).await;
     Template::render("ingredients_list", context!{
-        allergens: allergens
+        allergens: &allergens,
+        ingredients_list: &ingredients_list
     })
 }
 
 #[post("/ingredients_list/allergens", data = "<allergen>")]
-async fn add_new_allergen(db: &State<Database>, allergen: Form<Allergen>) {
+async fn add_new_allergen(db: &State<Database>, allergen: Form<Allergen>) -> Redirect {
     db.allergen.create(&allergen.into_inner()).await;
+    Redirect::to(uri!(ingredients_list))
+}
+
+#[delete("/ingredients_list/allergens/<id>")]
+async fn remove_allergen(db: &State<Database>, id: &str) -> Redirect {
+    db.allergen.delete(id).await;
+
+    Redirect::to(uri!(ingredients_list))
 }
 
 #[post("/ingredients_list/allergens/ingredient", data = "<allergen_ingredient>")]
@@ -94,7 +109,7 @@ async fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index, new_recipe, recipes, add_recipe, delete_recipe, 
                             new_ingredient, new_preparation_step, get_recipe_pdf, 
-                            ingredients_list, add_new_allergen, new_allergen_ingredient])
+                            ingredients_list, add_new_allergen, new_allergen_ingredient, remove_allergen])
         .mount("/public", FileServer::from(relative!("static")))
         .manage(db)
         .attach(Template::fairing())
