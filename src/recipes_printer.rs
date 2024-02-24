68bf022c39b4e137f0e5388f0d8e19c316409239
Junior Grégoire <junior.gregoire@gmail.com>
2024-02-24 03:53:14 -0500

    Print ingredients lists in pdf

--- src/recipes_printer.rs
+++ src/recipes_printer.rs
@@ -2,9 +2,13 @@ use tectonic;
 use crate::models::*;
 use crate::ingredient_list_manager::IngredientList;
 
-async fn header_latex() -> String {
-    let latex = r#"
-\documentclass[a4paper,17pt]{extarticle}
+async fn header_latex(pt: Option<String>) -> String {
+    let pt = pt.unwrap_or("17".to_owned());
+    let mut latex: String = r#"
+\documentclass[a4paper,"#.to_owned();
+
+    latex.push_str(&pt);
+    latex.push_str(r#"pt]{extarticle}
 \usepackage[T1]{fontenc}
 \usepackage{graphicx}
 \usepackage{fontspec}
@@ -20,14 +24,14 @@ async fn header_latex() -> String {
     \includegraphics[height=3cm]{template_pdf/banniere_benelux}
 \end{center}
 
-\section*{"#;
+\section*{"#);
 
     String::from(latex)
 }
 
 
 async fn generate_latex(recipe: &RecipeWithId) -> String {
-    let mut latex = header_latex().await;
+    let mut latex = header_latex(None).await;
     latex.push_str(&recipe.title);
     latex.push_str(r#"}
 \subsection*{Ingredients :}
@@ -56,9 +60,29 @@ async fn generate_latex(recipe: &RecipeWithId) -> String {
 }
 
 async fn generate_latex_for_ingredient_list(ingredients_lists: &Vec<IngredientList>) -> String {
-    let mut latex = header_latex().await;
-    latex.push_str("Listes des ingrédient");
+    let mut latex = header_latex(Some("15".to_owned())).await;
+    latex.push_str("Listes des ingrédients");
     latex.push_str(r#"}"#);
+    
+    for ingredients_list in ingredients_lists {
+        latex.push_str(&format!(r#"\subsection*{{{}}}"#, ingredients_list.recipe_name));
+        latex.push_str(r#"\subsubsection*{Ingrédients}"#);
+        
+        let mut ingredients_str = ingredients_list.ingredients.join(", ");
+            ingredients_str.push_str(".");
+            latex.push_str(&ingredients_str);
+
+            if ingredients_list.allergens.len() > 0 {
+            latex.push_str(r#"\subsubsection*{Allergène}"#);
+
+            let mut allergens_str = ingredients_list.allergens.join(", ");
+            allergens_str.push_str(".");
+
+            latex.push_str(&allergens_str);
+        }
+
+        latex.push_str(r#"\bigskip"#);
+    }
 
     latex.push_str(r#"
 \end{document}"#);
